548268585c088ad982f85035b23c683181f23b27
TheBoringDude <iamcoderx@gmail.com>
2021-05-21 22:38:08 +0800

    make list-draggin work

--- src/renderer/components/container/body.tsx
+++ src/renderer/components/container/body.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 
 import useCurrentProject from '../../hooks/useCurrentProject';
 import useWorkGroup from '../../hooks/useWorkGroup';
@@ -7,9 +7,27 @@ import ListGroup from './list-group';
 
 const ContainerBody = () => {
   const { selected } = useCurrentProject();
-  const { state } = useWorkGroup();
+  const { state, dispatch } = useWorkGroup();
 
-  const onDragEnd = () => {};
+  const onDragEnd = (result: DropResult) => {
+    const { destination: dest, source: src } = result;
+
+    if (!dest) return;
+    if (dest.droppableId === src.droppableId && dest.index === src.index)
+      return;
+
+    dispatch({
+      type: 'handle-drag',
+      source: {
+        id: src.droppableId,
+        index: src.index,
+      },
+      destination: {
+        id: dest.droppableId,
+        index: dest.index,
+      },
+    });
+  };
 
   return (
     <div className="py-8 px-3">
--- src/renderer/reducers/workgroups.ts
+++ src/renderer/reducers/workgroups.ts
@@ -8,7 +8,12 @@ type ActionsGroup =
   | { type: 'add'; id: string; group: ProjectWorkPropsContainer }
   | { type: 'remove'; id: string }
   | { type: 'set'; work: ProjectWorkProps }
-  | { type: 'add-list'; id: string; list: ProjectWorkCategoryProps[] };
+  | { type: 'add-list'; id: string; list: ProjectWorkCategoryProps[] }
+  | {
+      type: 'handle-drag';
+      source: { index: number; id: string };
+      destination: { index: number; id: string };
+    };
 
 const GroupReducer = (state: ProjectWorkProps, action: ActionsGroup) => {
   switch (action.type) {
@@ -35,6 +40,47 @@ const GroupReducer = (state: ProjectWorkProps, action: ActionsGroup) => {
           list: action.list,
         },
       };
+    case 'handle-drag': {
+      // TODO: It might need re-factoring.
+
+      const source = Array.from(state[action.source.id].list);
+
+      if (action.source.id === action.destination.id) {
+        const v = source[action.source.index];
+
+        source.splice(action.source.index, 1);
+        source.splice(action.destination.index, 0, v);
+
+        return {
+          ...state,
+          [action.source.id]: {
+            ...state[action.source.id],
+            list: source,
+          },
+        };
+      }
+
+      const destination = Array.from(state[action.destination.id].list);
+
+      const s = source[action.source.index];
+
+      const newSource = source.filter(
+        (_, index) => index !== action.source.index
+      );
+      destination.splice(action.destination.index, 0, s);
+
+      return {
+        ...state,
+        [action.source.id]: {
+          ...state[action.source.id],
+          list: newSource,
+        },
+        [action.destination.id]: {
+          ...state[action.destination.id],
+          list: destination,
+        },
+      };
+    }
 
     default:
       return state;
