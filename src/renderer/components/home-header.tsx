c2d3129f02b81958744998d32fb9a88d2a1797a6
TheBoringDude <iamcoderx@gmail.com>
2021-05-22 10:13:15 +0800

    do not truncate home-header

--- src/renderer/components/home-header.tsx
+++ src/renderer/components/home-header.tsx
@@ -4,10 +4,12 @@ const HomeHeader = () => {
   return (
     <>
       <div className="mb-2">
-        <h1 className="truncate text-2xl font-black tracking-wide text-indigo-500">
+        <h1 className="text-2xl font-black tracking-wide text-indigo-500">
           c2gin
         </h1>
-        <p className="truncate tracking-wide font-light mt-1">
+        <p className="tracking-wide font-light mt-1">
+          {/* TODO:
+          the plan in here is to get a random quote like a quote of the day if the user is connected to the internet */}
           a personal <strong>project</strong> management
         </p>
       </div>
